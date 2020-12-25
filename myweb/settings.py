7e303977a70f517ef3b4a8d550f85434ec84e7d5
Sohag Hossain <sh.oronno@live.com>
2020-12-25 13:25:59 +0300

    modified gitginore file and setup database

--- myweb/settings.py
+++ myweb/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'myweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'djangodb',
+        'USER':'postgres',
+        'PASSWORD':'73362',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
